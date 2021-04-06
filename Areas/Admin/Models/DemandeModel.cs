d178f24023a3f571a311b3445b0f943bf7e30c4a
Soukayna Bousfiha <soukabousfiha@gmail.com>
2021-04-06 12:18:16 +0000

    10C

---
+++ Areas/Admin/Models/DemandeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionAbscences.Areas.Admin.Models
+{
+    public class DemandeModel
+    {
+        public int IdTypeConge { get; set; }
+        public int matricule { get; set; }
+        public int IdDemandeConge { get; set; }
+
+        public String NomComplet { get; set; }
+
+        public String Classe { get; set; }
+        public string validationN1 { get; set; }
+        public string validationN2 { get; set; }
+
+        public DateTime DateDebut { get; set; }
+
+        public DateTime DateFin { get; set; }
+        public DateTime DateDc { get; set; }
+
+        public int nbJ { get; set; }
+    }
+}
