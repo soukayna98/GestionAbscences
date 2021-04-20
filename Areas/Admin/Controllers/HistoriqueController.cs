43d1bacad7b73edb4b4883f82884df42bf034b44
soukaina Sahmani <soukaina.sahmani@usmba.ac.ma>
2021-04-20 23:57:59 +0000

    25C

--- Areas/Admin/Controllers/HistoriqueController.cs
+++ Areas/Admin/Controllers/HistoriqueController.cs
@@ -21,7 +21,7 @@ namespace GestionAbscences.Areas.Admin.Controllers
             demandeService = new DemandeService();
         }
 
-       
+
         //private GestionAbscencesEntities1 db = new GestionAbscencesEntities1();
 
         // GET: Admin/Historique
@@ -46,58 +46,188 @@ namespace GestionAbscences.Areas.Admin.Controllers
                     IdDemandeConge = item.idDemandeConge,
                     NomComplet = item.employe.NomComplet
 
-                }) ;
+                });
             }
             return View(employesList);
         }
 
         
-        public ActionResult validation(int? id)
-        {
-            if (id == null)
+         public ActionResult validation(int? id)
+         {
+             if (id == null)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }
+            var currentDemande = demandeService.ReadById(id.Value);
+            if (currentDemande == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            demandeconge demandeconge = db.demandeconge.Find(id);
-            if (demandeconge == null)
-            {
-                return HttpNotFound();
+                return HttpNotFound($"this demande ({id}) is not found");
             }
            
-            return View(demandeconge);
-        }
-        [HttpPost]
-        public ActionResult Validation(demandeconge demandeconge)
+
+            demandeconge demandeconge = db.demandeconge.Find(id);
+            Session["uid"] = currentDemande.idDemandeConge;
+
+            if (demandeconge == null)
+             {
+                 return HttpNotFound();
+             }
+
+             return View(demandeconge);
+         }/*
+           [HttpPost]
+           public ActionResult Validation(demandeconge data)
+           {
+               if (ModelState.IsValid)
+               {
+                   string validation1 = Request.Form["validation1"];
+                   string validation2 = Request.Form["validation2"];
+
+                   int uid = data.IdEmploye;
+
+
+
+                   if (validation1.Equals("Accepté"))
+                   {
+                       Session["validation1"] = "Accepté";
+                   }
+                   else if (validation1.Equals("Refusé"))
+                   {
+                       Session["validation1"] = "Refusé";
+                   }
+
+                   if (validation2.Equals("Accepté"))
+                   {
+                       Session["validation2"] = "Accepté";
+                   }
+                   else if (validation2.Equals("Refusé"))
+                   {
+                       Session["validation2"] = "Refusé";
+                   }
+
+
+                   /*   {
+                          idDemandeConge = data.idDemandeConge,
+                          IdtypeConge = data.IdtypeConge,
+                          IdEmploye = data.IdEmploye,
+                          // d.NomComplet = data.Nom,
+                          // matricule = data.employe.matricule,
+                          DateDebut = (DateTime)data.DateDebut,
+                          DateFin = (DateTime)data.DateFin,
+                          DateDC = (DateTime)data.DateDC,
+                          ValidationN1 = Session["validation1"].ToString(),
+                          ValidationN2 = Session["validation1"].ToString(),
+                      };
+
+
+                   demandeconge updatedDemande = db.demandeconge.Find(uid);
+                   updatedDemande.ValidationN1 = Session["validation1"].ToString();
+                   updatedDemande.ValidationN2 = Session["validation2"].ToString();
+
+                   db.Entry(updatedDemande).State = EntityState.Modified;
+                   db.SaveChanges();
+
+
+               }
+
+               return View(data);
+           }
+
+
+        ///////////////////////////////
+        ///
+        //"Edit", "Edit", new { id = item.IdDemandeConge }
+        public ActionResult Validation(int? id)
         {
-            string validation1 = Request.Form["validation1"];
-            string validation2 = Request.Form["validation2"];
-            if (validation1.Equals("Accepté"))
+            if (id == null || id == 0)
             {
-                demandeconge.ValidationN1 = "Accepté";
-            }
-            else
-                  if (validation1.Equals("Refusé"))
-            {
-                demandeconge.ValidationN1 = "Refusé";
-            }
-            else if (validation2.Equals("Accepté"))
-            {
-                demandeconge.ValidationN2 = "Accepté";
-            }
-            else
-                  if (validation2.Equals("Refusé"))
-            {
-                demandeconge.ValidationN2 = "Refusé";
+                return RedirectToAction("Index", "Default");
             }
 
-            db.Entry(demandeconge).State = EntityState.Modified;
-            db.SaveChanges();
-            return RedirectToAction("Index", "Default");
+            var currentDemande = demandeService.ReadById(id.Value);
+            if (currentDemande == null)
+            {
+                return HttpNotFound($"this demande ({id}) is not found");
+            }
+            Session["uid"] = currentDemande.idDemandeConge;
 
+            var historiqueModel = new HistoriqueModel
+            {
+                IdDemande = currentDemande.idDemandeConge,
+                IdType = currentDemande.IdtypeConge,
+                //Nom = currentDemande.employe.NomComplet,
+                //matricule = currentDemande.employe.matricule,
+                DateD = (DateTime)currentDemande.DateDebut,
+                DateF = (DateTime)currentDemande.DateFin,
+                Datedc = (DateTime)currentDemande.DateDC,
+                validation1 = currentDemande.ValidationN1,
+                validation2 = currentDemande.ValidationN2,
+                IdEmploye = currentDemande.IdEmploye
+
+
+
+            };
+
+            return View(historiqueModel);
+        }*/
+
+        [HttpPost]
+        public ActionResult Validation()
+        {
+
+                string validation1 = Request.Form["validation1"];
+                string validation2 = Request.Form["validation2"];
+
+                int uid = int.Parse(Session["uid"].ToString());
+
+                demandeconge e = db.demandeconge.Find(uid);
+
+                Session["index"] = uid;
+
+                
+
+                if (validation2.Equals("Accepte"))
+                {
+                         e.ValidationN2 = "Accepté";
+                }
+                 if (validation2.Equals("Refuse"))
+                {
+                         e.ValidationN2 = "refusé";
+                }
+                 if (validation1.Equals("Accepte"))
+                {
+                e.ValidationN1 = "Accepté";
+                }
+            if (validation1.Equals("Refuse"))
+            {
+                e.ValidationN1 = "refusé";
+            }
+
+
+
+
+            db.Entry(e).State = EntityState.Modified;
+                db.SaveChanges();
+            
+
+
+
+
+            return RedirectToAction("Index" ,"Default");
         }
-               
+
+
+
+
     }
 }
+    
+               
+
+           
+               
+  
+
             
                 
         
